2d5e45348b4089394d7e304f3e9122cd24b10e1b
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-09-21 22:47:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
-    //Write your code here
+  // Get a reference to the form element
+  const form = document.getElementById("form1");
 
+  // Get the values of the First and Last name fields
+  const firstName = form.elements.fname.value;
+  const lastName = form.elements.lname.value;
+
+  // Display the values using an alert
+  alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
+  
+  // Prevent the default form submission
+  return false;
 }
